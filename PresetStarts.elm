9fd9ccd8a83436c6d8b45c7562590ca5d18d86fd
Geoffrey Ramseyer <ramseyer@uchicago.edu>
2016-02-15 15:38:16 -0600

    glider gun

--- PresetStarts.elm
+++ PresetStarts.elm
@@ -1,6 +1,56 @@
-module PresetStarts (blinker) where
+module PresetStarts (blinker, gliderGun) where
 
 import GameOfLife exposing (..)
 
+--taken from wikipedia
+
 blinker = fromListList [[False, True, False], [False, True, False], [False, True, False]]
+
+gliderGun = fromListList
+	[
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, True,  True,  False, False, False, False],
+		[False, False, False, False, False, True,  True,  False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, True,  True,  True,  False, False, False],
+		[False, False, False, False, True,  False, False, False, True,  False, False],
+		[False, False, False, True,  False, False, False, False, False, True,  False],
+		[False, False, False, True,  False, False, False, False, False, True,  False],
+
+		[False, False, False, False, False, False, True,  False, False, False, False],
+		[False, False, False, False, True,  False, False, False, True,  False, False],
+		[False, False, False, False, False, True,  True,  True,  False, False, False],
+		[False, False, False, False, False, False, True,  False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, True,  True,  True,  False, False, False, False, False],
+		[False, False, False, True,  True,  True,  False, False, False, False, False],
+		[False, False, True,  False, False, False, True,  False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+
+		[False, True,  True,  False, False, False, True,  True,  False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+		[False, False, False, True,  True,  False, False, False, False, False, False],
+		[False, False, False, True,  True,  False, False, False, False, False, False],
+		[False, False, False, False, False, False, False, False, False, False, False],
+
+	]
 
