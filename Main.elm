2df6f3608e14122393d4dc530978390d0a56ee8c
gramseyer <ramseyer@uchicago.edu>
2016-02-18 22:45:57 -0600

    Squares are now clickable

--- Main.elm
+++ Main.elm
@@ -3,6 +3,7 @@ import GameOfLife exposing(..)
 import PresetStarts exposing(..)
 import Graphics.Element as E
 import Graphics.Collage as C exposing(defaultLine)
+import Graphics.Input as I
 import Signal
 import Color
 import Array
@@ -10,6 +11,15 @@ import Time
 import Window
 
 --type alias State = (Grid, Bool) --this depends on the state of the game we're in, ie editing versus viewing mode
+
+type alias ClickEvent = (Int, Int)
+type Event = Tick | Click (Int, Int)
+
+lastClicked : Signal.Mailbox ClickEvent
+lastClicked = Signal.mailbox (0,0)
+
+maxGridX = 3
+maxGridY = 3
 
 getDimensions : Grid -> (Int, Int)
 getDimensions g = (Array.length g, Array.length (fromJust (Array.get 0 g)))
@@ -39,20 +49,35 @@ fromJust a = case a of
  Just a -> a
  Nothing -> Debug.crash "fromJust we had nothing"
 
+makeSquareForm : Color.Color -> Float -> C.Form
+makeSquareForm color size = (C.group [(C.filled color (C.square size)), (C.outlined {defaultLine | color = Color.blue} (C.square size)) ]) 
+
+makeFormIntoClickable : Int -> Int -> Int ->C.Form -> C.Form
+makeFormIntoClickable x y size form=
+    C.toForm (I.clickable (Signal.message lastClicked.address (x,y)) (C.collage size size [form]))
+
 makesquare : ((Int, Int), Bool, (Float, Int, Int)) -> C.Form
 makesquare ((x,y), v, (size, maxx, maxy))= 
  if v then
-  C.move ((0.5+(toFloat x)) *size - ((toFloat maxx)/2) , (0.5+ (toFloat y)) *size - ((toFloat maxy)/2)) (C.group [(C.filled Color.white (C.square size)), (C.outlined {defaultLine | color = Color.blue} (C.square size))])
+  C.move ((0.5+(toFloat x)) *size - ((toFloat maxx)/2) , (0.5+ (toFloat y)) *size - ((toFloat maxy)/2)) (makeFormIntoClickable x y (round size) (makeSquareForm Color.white size))
  else 
-  C.move (((0.5+(toFloat x)) *size - ((toFloat maxx)/2)) , (0.5+(toFloat y)) *size - ((toFloat maxy)/2)) (C.group [(C.filled Color.black (C.square size)), (C.outlined {defaultLine | color = Color.blue} (C.square size)) ]) 
+  C.move (((0.5+(toFloat x)) *size - ((toFloat maxx)/2)) , (0.5+(toFloat y)) *size - ((toFloat maxy)/2)) (makeFormIntoClickable x y (round size) (makeSquareForm Color.black size))
 
 -- List.map (\w-> List.map (\v -> (v, Array.get (snd v) ((Array.get (fst v) grid))))) (List.map (\f->List.map f [0..yMax]) (List.map (\x->(\y->(x,y))) [0..xMax]))
 
 
+tickSignal : Signal Event
+tickSignal = (Signal.map (\x-> Tick) (Time.every (Time.millisecond*500)))
 
-upstate : Time.Time -> Grid -> Grid
-upstate t g = updateGrid [0,1,4,5,6,7,8] [3] g 
+eventSignal : Signal Event
+eventSignal = (Signal.merge tickSignal (Signal.map (\(x,y)-> Click (x,y)) lastClicked.signal))
+
+upstate : Event -> Grid -> Grid
+upstate t g = 
+    case t of
+        Tick -> updateGrid [0,1,4,5,6,7,8] [3] g 
+        Click (x,y) -> Debug.log (toString (x,y)) g
 
 main : Signal E.Element
 main = Signal.map2 view Window.dimensions
- (Signal.foldp upstate gliderGun (Time.every (Time.millisecond )))
+ (Signal.foldp upstate blinker eventSignal)
