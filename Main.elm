c7691316962a5788bf9a35110e6a43f6f3d2a92a
hannahbrodheim <hannahbrodheim@gmail.com>
2016-02-19 11:36:12 -0600

    start and stop for editting added

--- Main.elm
+++ Main.elm
@@ -10,16 +10,17 @@ import Array
 import Time
 import Window
 
-type alias State = (Grid, Bool) --this depends on the state of the game we're in, ie editing versus viewing mode
+type alias UIControl = (Bool)
+type alias State = (Grid, UIControl) --this depends on the state of the game we're in, ie editing versus viewing mode
 
 type alias ClickEvent = (Int, Int)
-type Event = Tick | Click (Int, Int)
+type Event = Tick | Click (Int, Int) | UIStateChange (UIControl -> UIControl)
 
 lastClicked : Signal.Mailbox ClickEvent
 lastClicked = Signal.mailbox (0,0)
 
-maxGridX = 3
-maxGridY = 3
+statechange : Signal.Mailbox Event
+statechange = Signal.mailbox (UIStateChange identity)
 
 getDimensions : Grid -> (Int, Int)
 getDimensions g = (Array.length g, Array.length (fromJust (Array.get 0 g)))
@@ -27,15 +28,27 @@ getDimensions g = (Array.length g, Array.length (fromJust (Array.get 0 g)))
 -- view will generate a grid of squares black -> false, white -> true
 view : (Int, Int) -> State -> E.Element
 view (w,h) (g, bool) = 
--- if state then --if we are in viewing mode
+ E.flow E.down ((renderGrid (w,h) (g,bool)) :: [renderUI (w,h) (g,bool)])
+
+findsqsizewh : (Int, Int) -> Grid -> (Float, Int, Int)
+findsqsizewh (w,h) g = 
  let (maxx, maxy) = getDimensions g
  in 
 --the size of the square should be the smallest of the window/sizeofarray
- let sqsize = Basics.min ((toFloat w)/(toFloat maxx)) ((toFloat h)/(toFloat maxy))
- in
+ let sqsize = Basics.min ((toFloat w)/(toFloat maxx)) ((toFloat (h - 100))/(toFloat maxy))
+ in 
  let width = ceiling (sqsize * (toFloat maxx))
      height = ceiling (sqsize * (toFloat maxy))
  in
+ (sqsize, width, height)
+ 
+
+
+renderGrid : (Int, Int) -> State -> E.Element
+renderGrid (w,h) (g, bool) = 
+-- if state then --if we are in viewing mode
+ let (sqsize, width, height) = findsqsizewh (w,h) g
+ in 
  --create collage of appropriate size, not doing anything intelligent here yet and fill it with squares
 -- Debug.log (toString (sqsize,w,h,maxx,maxy, width, height))
  C.collage 
@@ -43,6 +56,13 @@ view (w,h) (g, bool) =
      ((C.filled Color.blue (C.rect (toFloat(width)) (toFloat(height)))) :: (List.concat (gridMapExtra makesquare (sqsize, width, height) g)))
 -- else
   --editing mode stuff
+
+renderUI : (Int, Int) -> State -> E.Element
+renderUI (w,h) (g,bool) = 
+ let (sqsize, wc, hc) = findsqsizewh (w,h) g
+ in
+ I.button (Signal.message statechange.address (UIStateChange (\x -> not x))) "Change Mode"
+
 
 fromJust : Maybe a -> a
 fromJust a = case a of 
@@ -69,16 +89,18 @@ makesquare ((x,y), v, (size, maxx, maxy))=
 tickSignal : Signal Event
 tickSignal = (Signal.map (\x-> Tick) (Time.every (Time.millisecond*500)))
 eventSignal : Signal Event
-eventSignal = (Signal.merge tickSignal (Signal.map (\(x,y)-> Click (x,y)) lastClicked.signal))
+eventSignal = (Signal.mergeMany [tickSignal, (Signal.map (\(x,y)-> Click (x,y)) lastClicked.signal), statechange.signal])
 
 upstate : Event -> State -> State
 upstate t (g, running) = 
     case t of
         Tick -> if running then (updateGrid [0,1,4,5,6,7,8] [3] g, running)
-                else (g, running)
+                else (g, running) 
+        UIStateChange f -> Debug.log (toString running) (g, f running)
         Click (x,y) -> if not running then
                         (toggleCoord (x,y) g, running)
                         else (g, running)
+
 
 main : Signal E.Element
 main = Signal.map2 view Window.dimensions
